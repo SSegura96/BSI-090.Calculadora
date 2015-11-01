31a692cb0ad05d50395712202569133b27cbf11e
Javier <Javier@WINDOWS-R7QBFHR>
2015-10-31 18:10:42 -0600

    Dia: 31/10/15 Se añaden menus al main.. todavia se sigue parando el github..

--- src/CalculadoraAvanzada/TestMain.java
+++ src/CalculadoraAvanzada/TestMain.java
@@ -6,64 +6,78 @@ import javax.swing.JOptionPane;
  *
  * @author Javier Fernandez & Sergio Segura
  */
-public class TestMain 
-{
-    public static void main(String[] args) 
-    {
+public class TestMain {
+
+    public static void main(String[] args) {
         boolean salir = false;
-        while (!salir) 
-        {
+        while (!salir) {
+            //Variable para alamacenar el indice del vector con la respuesta del JOption
             int indice = 0;
-            
+
             Object[] botonesMenuCalcu = {"Áreas de figuras planas(2D).",
                 "Perímetros de figuras planas (2D).", "Estadística.",
                 "Probabilidades y combinaciones.", "Superficies.", "Volúmenes.",
                 "Vectores y Matrices.", "Conjeturas."};
 
-            String elemento = JOptionPane.showInputDialog(null,
+            String elementoMenu1 = JOptionPane.showInputDialog(null,
                     "Digite el codigo de la opcion desesada: \n", "Calculadora Avanzada",
                     JOptionPane.PLAIN_MESSAGE, null, botonesMenuCalcu,
                     botonesMenuCalcu[0]).toString();
-            
-            for (int i = 0; i < botonesMenuCalcu.length; i++) 
-            {       
-                if (botonesMenuCalcu[i] == elemento)
-                {
-                    indice = i;
-                    i=botonesMenuCalcu.length;
-                }
-            }    
-            
-            System.out.println(indice);
 
-            /*
-             if (menuCalcu > 0 && menuCalcu <= 8)
-             {
-             switch (menuCalcu) 
-             {
-             case 1:
-             String[] bot = {"1)caca","2)diarrea","putrefaccion","cerote"};
-             JOptionPane.showInputDialog(null, "j", null, JOptionPane.INFORMATION_MESSAGE, null, bot, bot[0]);
-             {
-             short menuFigPlanas = Short.parseShort(JOptionPane.showInputDialog(null, 
-             "Digite el codigo de la figura deseada:\n"
-             + "\n 1) Circulo. \n 2) Triangulo."
-             + "\n 3) Cuadrado \n 4) Rectángulo"
-             + "\n 5) Rombo. \n 6)Trapecio. \n 7)Romboide."
-             + "\n 8) Polígono Regular", "Figuras Planas(2D)", 
-             JOptionPane.PLAIN_MESSAGE));
-             }//fin case 1
-                    
-             }//fin switch menuCalcu
-             }//fin primer if
-             else
-             JOptionPane.showMessageDialog(null, "Debe digitar numeros entre 1 y 8.", 
-             "Aviso", JOptionPane.WARNING_MESSAGE);
-             }//fin while
-        
-             System.exit(0);
-            
-             */
-        }
+            //Ciclo que obtiene el indice para usarlo el switch
+            for (int i = 0; i < botonesMenuCalcu.length; i++) {
+                if (botonesMenuCalcu[i] == elementoMenu1) {
+                    indice = i;
+                    i = botonesMenuCalcu.length;
+                }//fin if
+            }//fin for
+
+            switch (indice) {
+                case 0: {
+
+                    Object[] botonesMenuAreaFigPlanas = {"Circulo.", "Triangulo.",
+                        "Cuadrado.", "Rectángulo.", "Rombo.", "Trapecio.", "Romboide.",
+                        "Polígono Regular"};
+
+                    String menuFigPlanas = JOptionPane.showInputDialog(null,
+                            "Digite el codigo de la figura deseada:\n", "Areas Figuras Planas(2D)",
+                            JOptionPane.PLAIN_MESSAGE, null, botonesMenuAreaFigPlanas,
+                            botonesMenuAreaFigPlanas[0]).toString();
+
+                    //Ciclo que obtiene el indice para usarlo el switch
+                    for (int i = 0; i < botonesMenuAreaFigPlanas.length; i++) {
+                        indice = 0;
+                        if (botonesMenuAreaFigPlanas[i] == menuFigPlanas) {
+                            indice = i;
+                            i = botonesMenuAreaFigPlanas.length;
+                        }//fin if
+                    }//fin for
+                }//fin case 0
+
+                case 1: {
+                    Object[] botonesMenuPerimFigPlanas = {"Circulo.", "Triangulo.",
+                        "Cuadrado.", "Rectángulo.", "Rombo.", "Trapecio.", "Romboide.",
+                        "Polígono Regular"};
+
+                    String menuFigPlanas = JOptionPane.showInputDialog(null,
+                            "Digite el codigo de la figura deseada:\n", "Perimetros Figuras Planas(2D)",
+                            JOptionPane.PLAIN_MESSAGE, null, botonesMenuPerimFigPlanas,
+                            botonesMenuPerimFigPlanas[0]).toString();
+
+                    //Ciclo que obtiene el indice para usarlo el switch
+                    for (int i = 0; i < botonesMenuPerimFigPlanas.length; i++) 
+                    {
+                        indice = 0;
+                        if (botonesMenuPerimFigPlanas[i] == menuFigPlanas) 
+                        {
+                            indice = i;
+                            i = botonesMenuPerimFigPlanas.length;
+                        }//fin if
+                    }//fin for
+                }//fin case 1
+        }//fin switch menuCalcu
+    }//fin while
+
+    System.exit (0);
     }//fin main
 }//fin class TestMain
