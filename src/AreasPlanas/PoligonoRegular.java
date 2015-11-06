7162d6742b00f4a38f141569519575f62753995b
Sergio Segura Vidal <Sergio Segura Vidal@W10-RKOch3196-M>
2015-11-06 00:14:23 -0600

    01/11/15 Intento de push

--- src/AreasPlanas/PoligonoRegular.java
+++ src/AreasPlanas/PoligonoRegular.java
@@ -6,22 +6,5 @@ package AreasPlanas;
 
 public class PoligonoRegular 
 {
-    private double calcularApotema()
-    {
-        double angRadianes;
-        double angCentral;
-        double apotema;
-        int numLados = 0;
-        
-        //se saca el algulo central y lo convertimos a radianes para poder sacar la tangencia.
-        
-        angCentral = 360/numLados;
-        angRadianes = Math.toRadians(angCentral);
-        double medidaLado = 0;
-        
-        //Se saca el apotema: Lado/2*tan(angRadianes/2)
-
-        apotema = medidaLado/(2*Math.tan(angRadianes/2));
-        return apotema;
-    }//fin calcularApotema()
+   
 }
